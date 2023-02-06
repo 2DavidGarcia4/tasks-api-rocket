531bbd3dc2fd52e4e560217c9af010afeb99cabc
2DavidGarcia4 <gacub20ru@gmail.com>
2023-02-06 15:18:11 -0600

    update

--- migrations/2023-02-03-191126_create-tasks/up.sql
+++ migrations/2023-02-03-191126_create-tasks/up.sql
@@ -1,21 +1,5 @@
 -- Your SQL goes here
-CREATE TABLE "users" (
-  "id" uuid PRIMARY KEY,
-  "name" varchar(100) NOT NULL,
-  "email" varchar(30) NOT NULL,
-  "password" varchar NOT NULL,
-  "image_url" varchar(300)
-);
-
-CREATE TABLE "tasks" (
-  "id" uuid PRIMARY KEY,
-  "user_id" uuid NOT NULL,
-  "title" varchar NOT NULL,
-  "description" text NOT NULL,
-  "created_at" timestamp NOT NULL,
-  "notification_at" timestamp,
-  "completed_at" timestamp,
-  "is_completed" boolean DEFAULT false NOT NULL
-);
-
-ALTER TABLE "tasks" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE tasks
+ALTER COLUMN create_at SET DATA TYPE timestamp with time zone,
+ALTER COLUMN notification_at SET DATA TYPE timestamp with time zone,
+ALTER COLUMN completed_at SET DATA TYPE timestamp with time zone;
